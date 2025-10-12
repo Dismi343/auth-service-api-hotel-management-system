3bb641bfa8853432e231743dcdd875f351b86df5
Yushan Hettiarachchi <130626574+Dismi343@users.noreply.github.com>
2025-10-12 21:44:40 +0530

    patch-22

--- src/main/java/com/cpd/hotel_system/auth_service_api/api/UserController.java
+++ src/main/java/com/cpd/hotel_system/auth_service_api/api/UserController.java
@@ -1,10 +1,18 @@
 package com.cpd.hotel_system.auth_service_api.api;
 
 import com.cpd.hotel_system.auth_service_api.config.JwtService;
+import com.cpd.hotel_system.auth_service_api.dto.request.SystemUserRequestDto;
 import com.cpd.hotel_system.auth_service_api.service.SystemUserService;
+import com.cpd.hotel_system.auth_service_api.util.StandardResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("user-service/api/v1/users")
@@ -13,4 +21,22 @@ public class UserController {
 
     private final SystemUserService systemUserService;
     private final JwtService jwtService;
+
+    @PostMapping("/visitors/singup")
+    public ResponseEntity<StandardResponseDto> createUser(
+        @RequestBody SystemUserRequestDto dto
+        ){
+        try {
+            systemUserService.createUser(dto);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        201,
+                        "User created successfully",
+                        null
+                ),HttpStatus.CREATED
+        );
+    }
 }
